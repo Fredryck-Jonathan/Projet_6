290681170313ff828b368ea5bdee7abec7ce4385
Fredryck-Jonathan <102037006+Fredryck-Jonathan@users.noreply.github.com>
2022-09-24 00:49:58 +0200

    Reprise de tous le dossier

--- app.js
+++ app.js
@@ -1,64 +1,33 @@
 const express = require('express');
 
-
-const app = express();
-
-app.use(express.json());
-
-const Sauce = require('./models/sauce');
+const mongoose = require('mongoose');
 
 const path = require('path');
 
-const stuffRoutes = require('./routes/stuff');
-const userRoutes = require('./routes/user');
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-
-app.use('/api/sauces', (req, res, next) => {
-    Sauce.find()
-      .then(sauces => res.status(200).json(sauces))
-      .catch(error => res.status(400).json({ error }));
-});
-
-
-
-
-app.use('/api/sauces', stuffRoutes);
-app.use('/api/auth', userRoutes);
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
-
-
-const mongoose = require('mongoose');
+const app = express();
 
 mongoose.connect('mongodb+srv://FJ_P6_OC:Fredryck_P6@clusterp6.yarjkqu.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+const saucesRoutes = require('./routes/sauces')
+const userRoutes = require('./routes/user');
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
 
 module.exports = app;
