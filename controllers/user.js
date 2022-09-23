1bbc24189a4e3a2e65253dd9233f76a5f17a4f67
Fredryck-Jonathan <102037006+Fredryck-Jonathan@users.noreply.github.com>
2022-09-24 00:51:17 +0200

    Reprise de tous le dossier

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+
 const User = require('../models/User');
 
 
@@ -16,19 +17,18 @@ exports.signup = (req, res, next) => {
       .catch(error => res.status(500).json({ error }));
   };
 
-
   const jwt = require('jsonwebtoken');
 
-exports.login = (req, res, next) => {
+  exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ error: 'Paire identifiant/mot de passe incorrecte' });
+                return res.status(401).json({ error: 'Utilisateur non trouvé !' });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ error: 'Paire identifiant/mot de passe incorrecte' });
+                        return res.status(401).json({ error: 'Mot de passe incorrect !' });
                     }
                     res.status(200).json({
                         userId: user._id,
